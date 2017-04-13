0bd222907e435312a448a9e9d1c18ffff6d9b98c
kenichi SHIMIZU <Pooh@Sputnik.local>
2017-04-14 00:56:40 +0900

    add Xcode dummy project

---
+++ Xcode_dummy/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  Xcode_dummy
+//
+//  Created by 清水 健一 on 2017/04/13.
+//  Copyright © 2017年 清水 健一. All rights reserved.
+//
+
+#include "test.hpp"
---
+++ Xcode_dummy/test.hpp
@@ -0,0 +1,14 @@
+//
+//  test.hpp
+//  Xcode_dummy
+//
+//  Created by 清水 健一 on 2017/04/13.
+//  Copyright © 2017年 清水 健一. All rights reserved.
+//
+
+#ifndef test_hpp
+#define test_hpp
+
+#include <stdio.h>
+
+#endif /* test_hpp */
